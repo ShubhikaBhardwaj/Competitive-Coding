514dabecd19c5323a31e9cb18b109a6b57700cf6
Shubhika Bhardwaj <shubhikabhardwaj@gmail.com>
2020-06-06 12:20:38 +0530

    Challenge code added

--- CodeChef DSA series/tomandjerry.cpp
+++ CodeChef DSA series/tomandjerry.cpp
@@ -1,20 +1,43 @@
 #include<bits/stdc++.h>
 using  namespace std;
 
-int calwin(int t,int j)
+long long int  calwin(long long int  t,long long int  j)
 {
-   if(((t&1)==0) && ((j&1)==0))
-   {
-      return calwin(t/2,j/2);
-   }
- 
-   if(((t&1)==1) && ((j&1)==0))
-   {
-       return 1;
-   }
+   
+   long long int dp[t+1][j+1]={0};
 
+   for(long long int i=1;i<t+1;i++)
+        dp[i][1]=0;
 
-  else return 0;
+   for(long long int i=1;i<j+1;i++)
+       {
+           if((i&1)==0)//even
+            dp[1][i]=1;
+
+            else dp[1][i]=0; //odd
+       } 
+     
+  for(long long int i=2;i<t+1;i++)
+  {
+      for(long long int k=2;k<j+1;k++)
+      {
+          
+         if(((i&1)==0) && ((k&1)==0))
+           dp[i][k]+=dp[i/2][k/2];
+
+         else if(((i&1)==1) && ((k&1)==0))
+           dp[i][k]+=1;
+
+        
+      }
+  }
+
+  long long int ans=0;
+
+  for(long long int i=1;i<j+1;i++)
+  ans+=dp[t][i];
+
+  return ans;
 
 }
 
@@ -25,15 +48,14 @@ int main()
    cin>>t;
    while(t--)
    {
-       int n;
+       long long int n;
        cin>>n;
        
-       int totalwins=0;
-       for(int i=1;i<=n;i++)
-       {
-          totalwins+=calwin(n,i);
+       long long int totalwins;
+      
+       totalwins=calwin(n,n);
 
-       }
+    
     cout<<totalwins<<endl;
 
 
